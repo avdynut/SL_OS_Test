f12e1a2d4c2647ea00ef1b56dca131ed2fd99ba4
Andrey Arekhva <a.arekhva@gmail.com>
2022-07-27 17:31:34 +0300

    Add OpenSilver projects

---
+++ SL_OS_Test.Simulator/Startup.cs
@@ -0,0 +1,15 @@
+﻿using DemoProject;
+using OpenSilver.Simulator;
+using System;
+
+namespace SL_OS_Test.Simulator
+{
+    internal static class Startup
+    {
+        [STAThread]
+        static int Main(string[] args)
+        {
+            return SimulatorLauncher.Start(typeof(App));
+        }
+    }
+}
