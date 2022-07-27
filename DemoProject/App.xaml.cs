e19d0d0d883436b4d768c1f1b60df8499c44c202
Andrey Arekhva <a.arekhva@gmail.com>
2022-07-27 17:16:46 +0300

    Prepare SL project

--- DemoProject/App.xaml.cs
+++ DemoProject/App.xaml.cs
@@ -1,14 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
 
 namespace DemoProject
 {
@@ -17,21 +8,15 @@ namespace DemoProject
 
         public App()
         {
-            this.Startup += this.Application_Startup;
-            this.Exit += this.Application_Exit;
-            this.UnhandledException += this.Application_UnhandledException;
+            Startup += Application_Startup;
+            UnhandledException += Application_UnhandledException;
 
             InitializeComponent();
         }
 
         private void Application_Startup(object sender, StartupEventArgs e)
         {
-            this.RootVisual = new MainPage();
-        }
-
-        private void Application_Exit(object sender, EventArgs e)
-        {
-
+            RootVisual = new MainPage();
         }
 
         private void Application_UnhandledException(object sender, ApplicationUnhandledExceptionEventArgs e)
