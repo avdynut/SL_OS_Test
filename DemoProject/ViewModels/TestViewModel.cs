645f20143927a4b6a50658bf762d36c69facd484
Andrey Arekhva <a.arekhva@gmail.com>
2022-09-08 09:19:20 -0400

    Getting test property

--- DemoProject/ViewModels/TestViewModel.cs
+++ DemoProject/ViewModels/TestViewModel.cs
@@ -1,7 +1,20 @@
-﻿namespace DemoProject.ViewModels
+﻿using System.Diagnostics;
+
+namespace DemoProject.ViewModels
 {
     public class TestViewModel
     {
+        private int _number = 1;
+
+        public int TestProperty
+        {
+            get
+            {
+                Debug.WriteLine($"Call {_number}. {new StackTrace()}");
+                return _number++;
+            }
+        }
+
         public TestViewModel()
         {
         }
