6fbac8ca5f6e97e7b4e4c62a0bbf4781c2a64185
Andrey Arekhva <a.arekhva@gmail.com>
2022-09-08 13:03:56 -0400

    Add test with converter

---
+++ DemoProject/Converter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace DemoProject
+{
+    public class Converter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Debug.WriteLine($"Converter {value} {new StackTrace()}");
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
