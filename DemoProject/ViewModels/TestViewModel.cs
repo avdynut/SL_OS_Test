a717cde76039daa6e547a33e1daa1e1b32773d17
Andrey Arekhva <a.arekhva@gmail.com>
2022-09-09 09:12:16 -0400

    Impement INotifyPropertyChanged

--- DemoProject/ViewModels/TestViewModel.cs
+++ DemoProject/ViewModels/TestViewModel.cs
@@ -1,9 +1,18 @@
-﻿namespace DemoProject.ViewModels
+﻿using System.ComponentModel;
+
+namespace DemoProject.ViewModels
 {
-    public class TestViewModel
+    public class TestViewModel : INotifyPropertyChanged
     {
         public TestViewModel()
         {
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
