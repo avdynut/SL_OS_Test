3d6fe949d8a2db77ff7d256e1849a1d06d02be50
Andrey Arekhva <a.arekhva@gmail.com>
2022-09-16 09:08:48 -0400

    Additional test cases

--- DemoProject/MainPage.xaml.cs
+++ DemoProject/MainPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using DemoProject.ViewModels;
+using System.Collections.Specialized;
 using System.Diagnostics;
 using System.Windows.Controls;
 
@@ -12,6 +13,7 @@ namespace DemoProject
         {
             InitializeComponent();
             DataContext = _viewModel;
+            (itemsControl.Items as INotifyCollectionChanged).CollectionChanged += Items_CollectionChanged;
         }
 
         private void RadioButton_Checked(object sender, System.Windows.RoutedEventArgs e)
@@ -22,6 +24,16 @@ namespace DemoProject
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
             Debug.WriteLine($"Text changed to {(sender as TextBox).Text}");
+        }
+
+        private void Items_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            Debug.WriteLine($"Items count changed to {itemsControl.Items.Count}");
+        }
+
+        private void contentButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            Debug.WriteLine($"Content control {contentControl.Content}");
         }
     }
 }
