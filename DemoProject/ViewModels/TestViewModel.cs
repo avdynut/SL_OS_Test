8174edcdeffd0f51061a179137894b9ec57fe1fd
Andrey Arekhva <a.arekhva@gmail.com>
2022-09-16 01:52:41 -0400

    add test treeview

--- DemoProject/ViewModels/TestViewModel.cs
+++ DemoProject/ViewModels/TestViewModel.cs
@@ -1,11 +1,31 @@
-﻿using System.ComponentModel;
+﻿using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace DemoProject.ViewModels
 {
     public class TestViewModel : INotifyPropertyChanged
     {
+        public List<Item> Items { get; } = new List<Item>();
+
         public TestViewModel()
         {
+            for (int i = 0; i < 10; i++)
+            {
+                var item = new Item { Number = i };
+                Items.Add(item);
+
+                for (int j = 0; j < 3; j++)
+                {
+                    var childItem = new Item { Number = j };
+                    item.ChildItems.Add(childItem);
+
+                    for (int k = 0; k < 2; k++)
+                    {
+                        var subItem = new Item { Number = k };
+                        childItem.ChildItems.Add(subItem);
+                    }
+                }
+            }
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
@@ -14,5 +34,11 @@ namespace DemoProject.ViewModels
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
+    }
+
+    public class Item
+    {
+        public List<Item> ChildItems { get; } = new List<Item>();
+        public int Number { get; set; }
     }
 }
