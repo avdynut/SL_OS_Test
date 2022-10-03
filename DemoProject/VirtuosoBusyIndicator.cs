54586c623d6412024db005331a3690b7eaeb8aae
Andrey Arekhva <a.arekhva@gmail.com>
2022-10-03 10:09:55 -0500

    Add busy indicator

---
+++ DemoProject/VirtuosoBusyIndicator.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+
+namespace Virtuoso.Core.Controls
+{
+    public class VirtuosoBusyIndicator : BusyIndicator
+    {
+        private Storyboard sb;
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            sb = (Storyboard)GetTemplateChild("BusyAnimation");
+            sb.Completed += Sb_Completed;
+            SetBusyAnimationStoryboardFromIsBusy();
+        }
+
+        private void Sb_Completed(object sender, System.EventArgs e)
+        {
+            Debug.WriteLine("Completed");
+        }
+
+        protected override void OnIsBusyChanged(DependencyPropertyChangedEventArgs e)
+        {
+            base.OnIsBusyChanged(e);
+            SetBusyAnimationStoryboardFromIsBusy();
+        }
+        private void SetBusyAnimationStoryboardFromIsBusy()
+        {
+            if (IsBusy)
+                sb.Begin();
+            else
+                sb.Stop();
+        }
+    }
+}
