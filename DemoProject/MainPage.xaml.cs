48c0c8543daeadda230aca97a9f1658f827ebfb1
Andrey Arekhva <a.arekhva@gmail.com>
2022-11-21 06:32:53 -0600

    Add example

--- DemoProject/MainPage.xaml.cs
+++ DemoProject/MainPage.xaml.cs
@@ -1,16 +1,12 @@
-﻿using DemoProject.ViewModels;
-using System.Windows.Controls;
+﻿using System.Windows.Controls;
 
 namespace DemoProject
 {
     public partial class MainPage : UserControl
     {
-        private readonly TestViewModel _viewModel = new TestViewModel();
-
         public MainPage()
         {
             InitializeComponent();
-            DataContext = _viewModel;
         }
     }
 }
