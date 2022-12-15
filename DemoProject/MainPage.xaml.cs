6fd4605cf515296f4d554f0fb806542b2d557be2
Andrey Arekhva <a.arekhva@gmail.com>
2022-12-15 04:15:39 -0600

    Change effects

--- DemoProject/MainPage.xaml.cs
+++ DemoProject/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using DemoProject.ViewModels;
 using System.Windows.Controls;
+using System.Windows.Media.Effects;
 
 namespace DemoProject
 {
@@ -7,10 +8,29 @@ namespace DemoProject
     {
         private readonly TestViewModel _viewModel = new TestViewModel();
 
+        private int _radius = 2;
+
         public MainPage()
         {
             InitializeComponent();
             DataContext = _viewModel;
+        }
+
+        private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            textElement.Effect = new BlurEffect { Radius = _radius };
+            _radius += 2;
+        }
+
+        private void Button_Click_1(object sender, System.Windows.RoutedEventArgs e)
+        {
+            textElement.Effect = new DropShadowEffect { BlurRadius = _radius };
+            _radius += 2;
+        }
+
+        private void Button_Click_2(object sender, System.Windows.RoutedEventArgs e)
+        {
+            textElement.Effect = null;
         }
     }
 }
