935831cc87a0b2d07e65cf643ddf3e4246d17071
Andrey Arekhva <a.arekhva@gmail.com>
2022-12-19 05:41:53 -0600

    Scrolling is not working inside ContentControl

--- DemoProject/MainPage.xaml.cs
+++ DemoProject/MainPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using DemoProject.ViewModels;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace DemoProject
@@ -6,11 +7,25 @@ namespace DemoProject
     public partial class MainPage : UserControl
     {
         private readonly TestViewModel _viewModel = new TestViewModel();
+        private readonly ChildWindow1 _window = new ChildWindow1();
 
         public MainPage()
         {
             InitializeComponent();
             DataContext = _viewModel;
+
+            SizeChanged += MainPage_SizeChanged;
+        }
+
+        private void MainPage_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            _window.Width = e.NewSize.Width - 100;
+            _window.Height = e.NewSize.Height - 100;
+        }
+
+        private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            _window.Show();
         }
     }
 }
