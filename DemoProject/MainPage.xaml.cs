1df44486910999e414e38a57af6457be73120176
Andrey Arekhva <a.arekhva@gmail.com>
2022-12-27 08:41:51 +0300

    Brush converting

--- DemoProject/MainPage.xaml.cs
+++ DemoProject/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using DemoProject.ViewModels;
 using System.Windows.Controls;
+using System.Windows.Markup;
 
 namespace DemoProject
 {
@@ -11,6 +12,20 @@ namespace DemoProject
         {
             InitializeComponent();
             DataContext = _viewModel;
+
+            var xaml = @"        <Canvas xmlns=""http://schemas.microsoft.com/winfx/2006/xaml/presentation"" xmlns:x=""http://schemas.microsoft.com/winfx/2006/xaml"" Width=""46"" Height=""74.000001"">
+            <Path Stroke=""#ff000000""/>
+        </Canvas>";
+
+            try
+            {
+                var content = XamlReader.Load(xaml);
+            }
+            catch (System.Exception ex)
+            {
+
+                throw;
+            }
         }
     }
 }
