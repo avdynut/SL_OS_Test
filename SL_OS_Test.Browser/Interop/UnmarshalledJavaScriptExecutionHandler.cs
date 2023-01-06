d13006348fc4595f5dc967dd52ae9b21b093a794
Andrei Arekhva <a.arekhva@gmail.com>
2023-01-06 11:51:21 +0300

    Update packages

--- SL_OS_Test.Browser/Interop/UnmarshalledJavaScriptExecutionHandler.cs
+++ SL_OS_Test.Browser/Interop/UnmarshalledJavaScriptExecutionHandler.cs
@@ -3,7 +3,7 @@ using Microsoft.JSInterop;
 using Microsoft.JSInterop.WebAssembly;
 
 namespace SL_OS_Test.Browser.Interop;
-public class UnmarshalledJavaScriptExecutionHandler : IJavaScriptExecutionHandler
+public class UnmarshalledJavaScriptExecutionHandler : IWebAssemblyExecutionHandler
 {
     private const string MethodName = "callJSUnmarshalled";
     private readonly WebAssemblyJSRuntime _runtime;
@@ -21,5 +21,20 @@ public class UnmarshalledJavaScriptExecutionHandler : IJavaScriptExecutionHandle
     public object ExecuteJavaScriptWithResult(string javaScriptToExecute)
     {
         return _runtime.InvokeUnmarshalled<string, object>(MethodName, javaScriptToExecute);
+    }
+
+    public TResult InvokeUnmarshalled<T0, TResult>(string identifier, T0 arg0)
+    {
+        return _runtime.InvokeUnmarshalled<T0, TResult>(identifier, arg0);
+    }
+
+    public TResult InvokeUnmarshalled<T0, T1, TResult>(string identifier, T0 arg0, T1 arg1)
+    {
+        return _runtime.InvokeUnmarshalled<T0, T1, TResult>(identifier, arg0, arg1);
+    }
+
+    public TResult InvokeUnmarshalled<T0, T1, T2, TResult>(string identifier, T0 arg0, T1 arg1, T2 arg2)
+    {
+        return _runtime.InvokeUnmarshalled<T0, T1, T2, TResult>(identifier, arg0, arg1, arg2);
     }
 }
