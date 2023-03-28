822ddd679c2da136a5ab53202af71462842a2b4d
Andrei Arekhva <a.arekhva@gmail.com>
2023-03-28 13:22:03 +0300

    example of focus events

--- DemoProject/MainPage.xaml.cs
+++ DemoProject/MainPage.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using DemoProject.ViewModels;
+using System.Diagnostics;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace DemoProject
 {
@@ -11,6 +14,25 @@ namespace DemoProject
         {
             InitializeComponent();
             DataContext = _viewModel;
+
+            Debug.WriteLine($"Initialized, {GetFocusedElement()}");
+        }
+
+        private void OnLostFocus(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine($"OnLostFocus {sender}, {GetFocusedElement()}");
+        }
+
+        private void OnGotFocus(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine($"OnGotFocus {sender}, {GetFocusedElement()}");
+        }
+
+        private string GetFocusedElement()
+        {
+            var focusedElement = FocusManager.GetFocusedElement();
+            textBlock.Text = "Focused Element: " + focusedElement;
+            return textBlock.Text;
         }
     }
 }
