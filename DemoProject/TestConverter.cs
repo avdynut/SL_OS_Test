d05fcd4851c0966d99401102aa476ef97b67505a
Andrei Arekhva <a.arekhva@gmail.com>
2023-07-13 08:13:30 -0400

    Calling ConvertBack after Convert

---
+++ DemoProject/TestConverter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace DemoProject
+{
+    public class TestConverter : IValueConverter
+    {
+        public object Convert(
+            object value,
+            Type targetType,
+            object parameter,
+            CultureInfo culture)
+        {
+            Debug.WriteLine($"Convert {value ?? "null"}");
+            if (value == null)
+            {
+                return Visibility.Collapsed;
+            }
+
+            if (value.ToString().Trim() == "")
+            {
+                return Visibility.Collapsed;
+            }
+
+            if (value.ToString().Trim() == "Both")
+            {
+                return Visibility.Collapsed;
+            }
+
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(
+            object value,
+            Type targetType,
+            object parameter,
+            CultureInfo culture)
+        {
+            Debug.WriteLine($"ConvertBack {value}");
+            return null;
+        }
+    }
+}
--- DemoProject/ViewModels/TestViewModel.cs
+++ DemoProject/ViewModels/TestViewModel.cs
@@ -1,11 +1,25 @@
 ﻿using System.ComponentModel;
+using System.Diagnostics;
 
 namespace DemoProject.ViewModels
 {
     public class TestViewModel : INotifyPropertyChanged
     {
+        private object _currentMode;
+        public object CurrentMode
+        {
+            get { return _currentMode; }
+            set
+            {
+                Debug.WriteLine($"CurrentMode changed to {value ?? "null"}");
+                _currentMode = value;
+                OnPropertyChanged(nameof(CurrentMode));
+            }
+        }
+
         public TestViewModel()
         {
+            CurrentMode = "1";
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
